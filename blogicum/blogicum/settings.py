4a84907e8672a1cf37a7ccf7cd574dd0af434452
Igor Kolomytsev <igor.kolomitseff@yandex.ru>
2024-02-16 12:01:45 +0300

    Update Django templates settings

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -53,10 +53,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'blogicum.urls'
 
+TEMPLATES_DIR = BASE_DIR / 'templates'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
