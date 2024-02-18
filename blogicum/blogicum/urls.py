3a4844c38b2dca8b11e65ecbb832d03e8878de83
Igor Kolomytsev <igor.kolomitseff@yandex.ru>
2024-02-18 16:50:08 +0300

    Fix blogicum/urls.py

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -4,7 +4,7 @@ from django.urls import include, path
 
 
 urlpatterns = [
-    path('', include('blog.urls', namespace='blog')),
     path('pages/', include('pages.urls', namespace='pages')),
     path('admin/', admin.site.urls),
+    path('', include('blog.urls', namespace='blog')),
 ]
